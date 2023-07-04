662279cfe4edd2c99df2212e3d6d52ff334ee913
unknown <luka.duzaic1723@gmail.com>
2023-07-04 11:47:09 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,21 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    const formData = {};
+    Array.from(e.currentTarget.elements).forEach(field => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+
+    fetch('api/mail', {
+      method: 'post',
+      body: JSON.stringify(formData)
+    });
+    console.log(formData);
+  }
+
   return (
     <>
       <Head>
@@ -14,99 +29,50 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
+      <main className={styles.main}>
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+          <style jsx>{`
+            form {
+              font-size: 1.2em;
+            }
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+            label {
+              display: block;
+              margin-bottom: .2em;
+            }
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+            input,
+            textarea {
+              width: 100%;
+              padding: .8em;
+            }
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
+            button {
+              color: white;
+              font-size: 1em;
+              background-color: blueviolet;
+              padding: .8em 1em;
+              border: none;
+              border-radius: .2em;
+            }
+          `}</style>
+          <form onSubmit={handleOnSubmit}>
             <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
+              <label htmlFor="name">Name</label>
+              <input id="name" type="text" name="name" />
             </p>
-          </a>
+            <p>
+              <label htmlFor="email">Email</label>
+              <input id="email" type="text" name="email" />
+            </p>
+            <p>
+              <label htmlFor="message">Message</label>
+              <textarea id="message" name="message" />
+            </p>
+            <p>
+              <button>Submit</button>
+            </p>
+          </form>
         </div>
       </main>
     </>
